01a11a6cdc1c51b3c4cd5fa1a16c3a71123822b1
Simon Senic <simon.senic@kosickaakademia.sk>
2020-11-11 10:54:32 +0100

    Added Main, RWFile

---
+++ src/sk/kosickaacademic/simon/files/Main.java
@@ -0,0 +1,7 @@
+package sk.kosickaacademic.simon.files;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/sk/kosickaacademic/simon/files/RWFile.java
@@ -0,0 +1,19 @@
+package sk.kosickaacademic.simon.files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class RWFile {
+    public void copyFileWithCapital(String fileName){
+        BufferedReader reader;
+        try{
+            FileReader fr = new FileReader();
+            
+
+
+
+
+        }
+
+    }
+}
