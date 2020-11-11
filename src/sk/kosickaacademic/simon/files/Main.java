96919b316390d5e3ea834a4538dd9fbbc58148e0
Simon Senic <simon.senic@kosickaakademia.sk>
2020-11-11 17:41:23 +0100

    Updated Main, RWFile

--- src/sk/kosickaacademic/simon/files/Main.java
+++ src/sk/kosickaacademic/simon/files/Main.java
@@ -2,6 +2,7 @@ package sk.kosickaacademic.simon.files;
 
 public class Main {
     public static void main(String[] args) {
-
+        RWFile rwf = new RWFile();
+        rwf.copyFileWithCapital("resources/a.txt");
     }
 }
--- src/sk/kosickaacademic/simon/files/RWFile.java
+++ src/sk/kosickaacademic/simon/files/RWFile.java
@@ -1,19 +1,50 @@
 package sk.kosickaacademic.simon.files;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class RWFile {
     public void copyFileWithCapital(String fileName){
         BufferedReader reader;
+        BufferedWriter writer;
         try{
-            FileReader fr = new FileReader();
-            
-
-
-
-
+            reader = new BufferedReader(new FileReader(fileName));
+            File file = new File(createFile());
+            writer = new BufferedWriter(new FileWriter(file));
+            String line = reader.readLine();
+            while(line!=null){
+                line=checkChar(line);
+                writer.write(line +'\n');
+                line=reader.readLine();
+            }
+            reader.close();
+            writer.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
         }
-
     }
+
+    public String createFile(){
+        String name="output/b_";
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HHmmss");
+        String timeX = time.format(timeFormat);
+
+        return name=name+timeX;
+    }
+
+    public String checkChar(String a){
+        String result="";
+        for(int i=0; i<a.length(); i++){
+            if(a.charAt(i)=='i' || a.charAt(i)=='y' || a.charAt(i)=='I' || a.charAt(i)=='Y')
+                result+='_';
+            else
+                result+=a.charAt(i);
+        }
+        return result;
+    }
+
 }
